0f387330b1ff4e1895ad2464f50adff9d38a7de9
pbl <9574047+leixen@user.noreply.gitee.com>
2023-10-13 14:55:57 +0800

    补充定义

--- preset/cortex-m4.cmake
+++ preset/cortex-m4.cmake
@@ -1,2 +1,4 @@
-set(EMLIB_ARCH_FLAGS "-mfpu=fpv4-sp-d16")
-include(preset/thumbv7em-none-eabihf.cmake)
+if(NOT EMLIB_ARCH_FLAGS)
+	set(EMLIB_ARCH_FLAGS "-mfpu=fpv4-sp-d16")
+endif(NOT EMLIB_ARCH_FLAGS)
+include(preset/thumbv7em-none-eabi.cmake)
