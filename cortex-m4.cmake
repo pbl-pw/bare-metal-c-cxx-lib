0df72f2421622d63f76f12b07fc8af1719688f7e
pbl <9574047+leixen@user.noreply.gitee.com>
2023-10-13 12:14:47 +0800

    添加m4配置

---
+++ cortex-m4.cmake
@@ -0,0 +1,2 @@
+set(EMLIB_ARCH_FLAGS "-mfpu=fpv4-sp-d16")
+include(thumbv7em-none-eabihf.cmake)
--- thumbv7m-none-eabi.cmake
+++ thumbv7m-none-eabi.cmake
@@ -1,3 +1,4 @@
 set(EMLIB_ARCH "armv7m")
 set(EMLIB_ABI "eabi")
 set(EMLIB_ARCH_FLAGS "-march=armv7m -mfloat-abi=soft -mfpu=none")
+set(tripple_hash "armv7m-soft-none")
